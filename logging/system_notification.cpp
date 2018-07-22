210b59ba95bcf5384ab40c8a6fd3224f8530c3b2
CollinAvidano <collin.avidano@gmail.com>
2018-07-22 17:40:11 -0400

    WIP: Started Template for System Notification class

---
+++ logging/system_notification.cpp
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <iostream>
+//TODO: Improve this class to be a more comprehensive system notification platform
+// current it only displays very specific popups from python.
+
+using namespace std;
+
+/**
+ * Adds the notification to a queue to be displayed
+ *
+ *
+ */
+void notify(const std::string& message) {
+    
+}
+
+/**
+ * Immediately displays the notification. Canceling any currently displayed
+ * notification re adding it to the queue
+ *
+ */
+void notifyNow(const std::string& message) {
+    
+}
+
+void displayNotification() {
+    
+}
+
